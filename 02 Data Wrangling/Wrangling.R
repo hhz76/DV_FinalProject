b3ff145312e0ffebcfe1f0eda706f787353ad9ef
unknown <henryzhao@HENRYZHAO-T420.TheFacebook.com>
2015-05-12 22:44:51 -0500

    Adding Henry's files.

---
+++ 02 Data Wrangling/Wrangling.R
@@ -0,0 +1,17 @@
+library("bitops")
+library("gdata")
+library("ggplot2")
+library("gplots")
+library("grid")
+library("jsonlite")
+library("knitr")
+library("plyr")
+library("RCurl")
+library("reshape2")
+library("rmarkdown")
+library("tidyr")
+library("dplyr")
+
+births %>% group_by(zip_code) %>% summarise(avg_birth = mean(total_births)) %>% ggplot(aes(x = zip_code, y = avg_birth)) + geom_boxplot()
+
+deaths %>% group_by(zip_code) %>% summarise(ave_death = mean(total_deaths)) %>% ggplot(aes(x=zip_code, y=ave_death)) + geom_boxplot()
